87e80dda3ca60ca044652bf734760c972901940b
DaniilShmoylove <88559460+DaniilShmoylove@users.noreply.github.com>
2022-03-18 19:21:33 +0300

    Initial Commit

---
+++ ScrollableTabBar/ScrollableTabBar.h
@@ -0,0 +1,18 @@
+//
+//  ScrollableTabBar.h
+//  ScrollableTabBar
+//
+//  Created by Daniil Shmoylove on 18.03.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScrollableTabBar.
+FOUNDATION_EXPORT double ScrollableTabBarVersionNumber;
+
+//! Project version string for ScrollableTabBar.
+FOUNDATION_EXPORT const unsigned char ScrollableTabBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollableTabBar/PublicHeader.h>
+
+
